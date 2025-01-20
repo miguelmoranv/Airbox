6cd1728401e607e111155f7878dfc9a2d627b1b0
DavidDavilaDev <daviddavilabarrios@gmail.com>
2025-01-20 09:13:33 -0600

    user-deleted

--- frontairbox/src/App.js
+++ frontairbox/src/App.js
@@ -1,7 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { UserProvider } from './contexts/UserContext';
 import Main from './views/login';
 
 function App() {
