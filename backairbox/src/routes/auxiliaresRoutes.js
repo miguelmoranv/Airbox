07de8b2c8f8566fbe0e4c03609fba969fd76ce72
miguelmoranv <miguelonchas001@gmail.com>
2025-03-12 23:51:47 -0600

    verifyTokenRoutes

--- backairbox/src/routes/auxiliaresRoutes.js
+++ backairbox/src/routes/auxiliaresRoutes.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const auxiliaresController = require('../controllers/auxiliaresController'); // Ajusta la ruta según tu estructura
-
+const verifyToken = require("../middlewares/verifyToken")
 // Rutas para la tabla "auxiliares"
-router.get('/', auxiliaresController.getAuxiliares); // Obtener todos los auxiliares
-router.get('/:id', auxiliaresController.getAuxiliarById); // Obtener un auxiliar por ID
-router.post('/', auxiliaresController.createAuxiliar); // Crear un nuevo auxiliar
-router.put('/:id', auxiliaresController.updateAuxiliar); // Actualizar un auxiliar por ID
-router.delete('/:id', auxiliaresController.deleteAuxiliar); // Eliminar un auxiliar por ID
-
+router.get('/', verifyToken, auxiliaresController.getAuxiliares); // Obtener todos los auxiliares
+router.get('/:id', verifyToken, auxiliaresController.getAuxiliarById); // Obtener un auxiliar por ID
+router.post('/', verifyToken, auxiliaresController.createAuxiliar); // Crear un nuevo auxiliar
+router.put('/:id', verifyToken, auxiliaresController.updateAuxiliar); // Actualizar un auxiliar por ID
+router.delete('/:id', verifyToken, auxiliaresController.deleteAuxiliar); // Eliminar un auxiliar por ID
+ 
 
 router.get('/users/:fg_users', auxiliaresController.getAuxiliarByFgUser)
 
--- backairbox/src/routes/cajasRoutes.js
+++ backairbox/src/routes/cajasRoutes.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const cajasController = require('../controllers/cajasController'); // Ajusta la ruta según tu estructura
-
+const verifyToken = require('../middlewares/verifyToken');
 // Rutas para la tabla "cajas"
-router.get('/', cajasController.getCajas); // Obtener todas las cajas
-router.get('/:id', cajasController.getCajaById); // Obtener una caja por ID
-router.post('/', cajasController.createCaja); // Crear una nueva caja
-router.put('/:id', cajasController.updateCaja); // Actualizar una caja por ID
-router.delete('/:id', cajasController.deleteCaja); // Eliminar una caja por ID
+router.get('/', verifyToken, cajasController.getCajas); // Obtener todas las cajas
+router.get('/:id', verifyToken, cajasController.getCajaById); // Obtener una caja por ID
+router.post('/', verifyToken, cajasController.createCaja); // Crear una nueva caja
+router.put('/:id', verifyToken, cajasController.updateCaja); // Actualizar una caja por ID
+router.delete('/:id', verifyToken, cajasController.deleteCaja); // Eliminar una caja por ID
 
-router.get('/lote/:id', cajasController.getCajaByLote )
+router.get('/lote/:id', verifyToken, cajasController.getCajaByLote )
 module.exports = router;
